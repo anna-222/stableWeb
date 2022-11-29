8af8158345c3fbfcf813b4208be199185e008f01
Anna Sawińska <a.sawinska222@gmail.com>
2022-11-29 23:17:30 +0100

    update pagination

--- src/pages/Pagination.js
+++ src/pages/Pagination.js
@@ -1,56 +1,56 @@
-import React from 'react'
-import '../pagination.css'
-import { useState, useEffect } from 'react';
+import React from "react";
+import "../pagination.css";
+import { useState, useEffect } from "react";
 
+function Pagination({ postPerPage, totalPosts, paginate }) {
+  const pageNumbers = [];
+  const [oldPage, setOldPage] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [number, setNumber] = useState(1);
+  // console.log('postPerPage:',postPerPage,'totalPosts',totalPosts)
 
-function Pagination({postPerPage, totalPosts, paginate}) {
-    const pageNumbers = [];
-    const [oldPage, setOldPage]= useState()
-    const [currentPage, setCurrentPage]= useState(1)
-    const [number, setNumber] = useState(1)
-    // console.log('postPerPage:',postPerPage,'totalPosts',totalPosts)
+  for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-    for(let i=1; i <= Math.ceil(totalPosts /postPerPage); i++){
-        pageNumbers.push(i)
+  console.log("page", pageNumbers);
+
+  useEffect(() => {
+    const oldPageNumber = document.querySelector(".activePage");
+    if (oldPageNumber) {
+      console.log(oldPageNumber);
+      oldPageNumber.classList.remove("activePage");
     }
 
-    console.log("page",pageNumbers);
-
-    
-       useEffect(()=>{  
-          const oldPageNumber = document.querySelector('.activePage')
-          if(oldPageNumber){
-          console.log(oldPageNumber)
-          oldPageNumber.classList.remove('activePage')}
-        const page = document.getElementById(`${number}`)
-        page.classList.add('activePage')
-      
-      
-      },[currentPage])
-  
-    
-
+    const page = document.getElementById(`${number}`);
+    if (page) {
+      page.classList.add("activePage");
+    }
+  }, [currentPage]);
 
   return (
-   <div className='pagination'>
-     <ul>
-        {pageNumbers.map(number=>{
-            return(
-            <li className='page' key={number}>
-               <a id={number} onClick={()=>{
-                paginate(number)
-                setNumber(number)
-                setCurrentPage(number)}
-              }
-                
-                href="#">
-                    {number}
-                </a> 
-            </li>)
+    <div className="pagination">
+      <ul>
+        {pageNumbers.map((number) => {
+          return (
+            <li className="page" key={number}>
+              <a
+                id={number}
+                onClick={() => {
+                  paginate(number);
+                  setNumber(number);
+                  setCurrentPage(number);
+                }}
+                href="#"
+              >
+                {number}
+              </a>
+            </li>
+          );
         })}
-     </ul>
-   </div>
-  )
+      </ul>
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
